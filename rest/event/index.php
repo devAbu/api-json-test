b8844f960ab112242185dd55bdc8ae4bbb671db8
Abdurahman Almonajed <abud_almonajed@hotmail.com>
2018-06-23 22:37:11 +0200

    testing

--- rest/event/index.php
+++ rest/event/index.php
@@ -2,15 +2,18 @@
 <html>
 <body>
     <p id="demo"></p>
+    <p id="demo2"></p>
 
     <script>
         var obj, dbParam, xmlhttp;
-        obj = { "table":"event", "limit":10 };
+        obj = { "table":"event", "limit":2 };
         dbParam = JSON.stringify(obj);
         xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 document.getElementById("demo").innerHTML = this.responseText;
+                 var myObj = JSON.parse(this.responseText);
+                document.getElementById("demo2").innerHTML = myObj[0].id;
             }
         };
         xmlhttp.open("GET", "getData.php?x=" + dbParam, true);
